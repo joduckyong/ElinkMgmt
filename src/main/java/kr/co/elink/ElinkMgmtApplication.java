140563102bf817f15d52a03c76ac17c09704f972
joduckyong <dyddl7609@gmail.com>
2022-12-21 15:22:09 +0900

    first commit

---
+++ src/main/java/kr/co/elink/ElinkMgmtApplication.java
@@ -0,0 +1,13 @@
+package kr.co.elink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElinkMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElinkMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/elink/ElinkMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.elink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElinkMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
