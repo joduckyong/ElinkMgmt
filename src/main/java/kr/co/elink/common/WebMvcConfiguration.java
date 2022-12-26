58a06ac296828cd01013ac4023ef5286b8f6782d
joduckyong <dyddl7609@gmail.com>
2022-12-26 14:58:39 +0900

    Put 안되는 현상 수정

--- src/main/java/kr/co/elink/common/WebMvcConfiguration.java
+++ src/main/java/kr/co/elink/common/WebMvcConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -25,7 +26,13 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*");
+                .allowedOrigins("*")    
+                .allowedMethods(
+                		HttpMethod.HEAD.name(),
+                    	HttpMethod.GET.name(),
+                    	HttpMethod.POST.name(),
+                    	HttpMethod.PUT.name(),
+                    	HttpMethod.DELETE.name());
     }
     
 	@Override
