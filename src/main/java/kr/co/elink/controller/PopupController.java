721816b153e6287d91246eddfaa1e59d6c6b1b42
joduckyong <dyddl7609@gmail.com>
2022-12-28 14:22:34 +0900

    api 수정

--- src/main/java/kr/co/elink/controller/PopupController.java
+++ src/main/java/kr/co/elink/controller/PopupController.java
@@ -1,9 +1,13 @@
 package kr.co.elink.controller;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.co.elink.common.StatusEnum;
+import kr.co.elink.dto.MessageVo;
 import kr.co.elink.dto.PopupVo;
 import kr.co.elink.service.PopupService;
 
@@ -25,38 +31,105 @@ public class PopupController {
 	PopupService popupService;
 	
 	@GetMapping("/{id}/{pageIndex}")
-    public ResponseEntity<List<PopupVo>> selectPopup(@PathVariable("id") String id, @PathVariable("pageIndex") int pageIndex) {
-        List<PopupVo> selectPopup = popupService.selectPopup(id, pageIndex);
-        return ResponseEntity.status(HttpStatus.OK).body(selectPopup);
+    public ResponseEntity<MessageVo> selectPopup(@PathVariable("id") String id, @PathVariable("pageIndex") int pageIndex) {
+        List<PopupVo> list = popupService.selectPopup(id, pageIndex);
+        int totalCount = 0;
+        if(list.size() > 0) {
+        	totalCount = list.get(0).getTotalCount();
+        }
+        
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        MessageVo message = MessageVo.builder()
+        	.status(StatusEnum.OK)
+        	.message("성공 코드")
+        	.totalCount(totalCount)
+        	.data(list)
+        	.build();
+        
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
     
     @GetMapping("/{id}")
-    public ResponseEntity<PopupVo> selectPopupInfo(@PathVariable("id") String id) {
+    public ResponseEntity<MessageVo> selectPopupInfo(@PathVariable("id") String id) {
     	PopupVo selectPopupInfo = popupService.selectPopupInfo(id);
-        return ResponseEntity.status(HttpStatus.OK).body(selectPopupInfo);
+    	
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        MessageVo message = MessageVo.builder()
+            	.status(StatusEnum.OK)
+            	.message("성공 코드")
+            	.totalCount(1)
+            	.data(selectPopupInfo)
+            	.files(new ArrayList<>())
+            	.build();
+        
+    	return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
     
     @PostMapping("")
-    public ResponseEntity<Integer> insertPopup(@RequestBody PopupVo popupVo) {
+    public ResponseEntity<MessageVo> insertPopup(@RequestBody PopupVo popupVo) {
     	int insertPopup = popupService.insertPopup(popupVo);
-        return ResponseEntity.status(HttpStatus.OK).body(insertPopup);
+    	
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        MessageVo message = MessageVo.builder()
+            	.status(StatusEnum.OK)
+            	.message("성공 코드")
+            	.data(insertPopup)
+            	.build();
+    	
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }    
     
     @PutMapping("")
-    public ResponseEntity<Integer> updatePopup(@RequestBody PopupVo popupVo) {
+    public ResponseEntity<MessageVo> updatePopup(@RequestBody PopupVo popupVo) {
     	int updatePopup = popupService.updatePopup(popupVo);
-        return ResponseEntity.status(HttpStatus.OK).body(updatePopup);
+    	
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        MessageVo message = MessageVo.builder()
+            	.status(StatusEnum.OK)
+            	.message("성공 코드")
+            	.data(updatePopup)
+            	.build();
+        
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }    
     
     @DeleteMapping("/{id}")
-    public ResponseEntity<Integer> deletePopup(@PathVariable("id") String id) {
+    public ResponseEntity<MessageVo> deletePopup(@PathVariable("id") String id) {
     	int deletePopup = popupService.deletePopup(id);
-        return ResponseEntity.status(HttpStatus.OK).body(deletePopup);
+
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        MessageVo message = MessageVo.builder()
+            	.status(StatusEnum.OK)
+            	.message("성공 코드")
+            	.data(deletePopup)
+            	.build();
+        
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
     
 	@DeleteMapping("")
-	public ResponseEntity<Integer> deletePopupIds(@RequestBody PopupVo popupVo) throws Exception{
+	public ResponseEntity<MessageVo> deletePopupIds(@RequestBody PopupVo popupVo) throws Exception{
     	int deletePopupIds = popupService.deletePopupIds(popupVo);
-        return ResponseEntity.status(HttpStatus.OK).body(deletePopupIds);
+
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        MessageVo message = MessageVo.builder()
+            	.status(StatusEnum.OK)
+            	.message("성공 코드")
+            	.data(deletePopupIds)
+            	.build();
+        
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);	
 	}
 }
