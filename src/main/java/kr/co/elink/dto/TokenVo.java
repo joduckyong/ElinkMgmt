4b41c45904465964b4f11f90f5828ac36c4bf03b
joduckyong <dyddl7609@gmail.com>
2022-12-29 15:20:33 +0900

    JWT 인증 개발

---
+++ src/main/java/kr/co/elink/dto/TokenVo.java
@@ -0,0 +1,14 @@
+package kr.co.elink.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class TokenVo {
+	private String grantType;
+	private String accessToken;
+//	private String refreshToken;
+}
