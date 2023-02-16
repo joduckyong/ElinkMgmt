b3f0b864c3a9ae8e4f617e469b97551de883e35c
joduckyong <dyddl7609@gmail.com>
2023-02-16 13:40:17 +0900

    수정중

---
+++ src/main/java/kr/co/elink/ElinkMgmtApplication.java
@@ -0,0 +1,14 @@
+package kr.co.elink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ElinkMgmtApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElinkMgmtApplication.class, args);
+	}
+
+}
