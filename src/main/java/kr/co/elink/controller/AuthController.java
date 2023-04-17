4f89a721b5d0c78522a1609f7853f8cc1e6b5a61
joduckyong <dyddl7609@gmail.com>
2023-04-17 17:00:55 +0900

    파일명 수정

--- src/main/java/kr/co/elink/controller/AuthController.java
+++ src/main/java/kr/co/elink/controller/AuthController.java
@@ -1,56 +1,36 @@
 package kr.co.elink.controller;
 
-import java.nio.charset.Charset;
-import java.util.Map;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.co.elink.common.ApiService;
-import kr.co.elink.common.StatusEnum;
-import kr.co.elink.dto.MessageVo;
+import kr.co.elink.dto.AdminVo;
+import kr.co.elink.dto.TokenVo;
+import kr.co.elink.service.AuthService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/ev/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
-    @Value("${ev.api.url}")
-    private String evApiUrl;	
-    
 	@Autowired
-	private ApiService<?> apiService;
-	
-	@ResponseBody
-	@PostMapping("{apiId}")
-	public ResponseEntity<MessageVo> auth(@RequestBody Map<String, Object> param, HttpSession session, ModelMap model) throws Exception{
-		
-		String url = evApiUrl+param.get("url");
-		
-		ResponseEntity<?> responseEntity = apiService.post(url, param);
-		
-        HttpHeaders headers= new HttpHeaders();
-        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+	AuthService authService;
 
-        MessageVo message = MessageVo.builder()
-            	.status(StatusEnum.OK)
-            	.message("성공 코드")
-            	.data(responseEntity.getBody())
-            	.build();
-    	
-        return new ResponseEntity<>(message, headers, HttpStatus.OK);
-	}	
+	@PostMapping("")
+	public TokenVo login(@RequestBody AdminVo adminVo) {
+
+		TokenVo tokenVo = authService.login(adminVo);
+		
+		return tokenVo;
+	}
+
+	@PostMapping(value = "/logout")
+	public String logout() {
+		
+		return "logout";
+		
+	}
 }
--- src/main/java/kr/co/elink/controller/EVAuthController.java
+++ src/main/java/kr/co/elink/controller/EVAuthController.java
@@ -1,36 +1,56 @@
 package kr.co.elink.controller;
 
+import java.nio.charset.Charset;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import kr.co.elink.dto.AdminVo;
-import kr.co.elink.dto.TokenVo;
-import kr.co.elink.service.AuthService;
+import kr.co.elink.common.ApiService;
+import kr.co.elink.common.StatusEnum;
+import kr.co.elink.dto.MessageVo;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/auth")
-public class AuthController {
+@RequestMapping("/api/ev/auth")
+public class EVAuthController {
 
+    @Value("${ev.api.url}")
+    private String evApiUrl;	
+    
 	@Autowired
-	AuthService authService;
-
-	@PostMapping("")
-	public TokenVo login(@RequestBody AdminVo adminVo) {
-
-		TokenVo tokenVo = authService.login(adminVo);
+	private ApiService<?> apiService;
+	
+	@ResponseBody
+	@PostMapping("{apiId}")
+	public ResponseEntity<MessageVo> auth(@RequestBody Map<String, Object> param, HttpSession session, ModelMap model) throws Exception{
 		
-		return tokenVo;
-	}
+		String url = evApiUrl+param.get("url");
+		
+		ResponseEntity<?> responseEntity = apiService.post(url, param);
+		
+        HttpHeaders headers= new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 
-	@PostMapping(value = "/logout")
-	public String logout() {
-		
-		return "logout";
-		
-	}
+        MessageVo message = MessageVo.builder()
+            	.status(StatusEnum.OK)
+            	.message("성공 코드")
+            	.data(responseEntity.getBody())
+            	.build();
+    	
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
+	}	
 }
